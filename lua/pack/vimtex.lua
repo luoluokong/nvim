4a8b5f6df2a8eafe43e0fbece34e732294102643
luoluokong <luoluokongqaq@gmail.com>
2023-04-02 12:38:29 +0800

    add snippets for markdown && modify vimtex config

--- lua/pack/vimtex.lua
+++ lua/pack/vimtex.lua
@@ -7,6 +7,7 @@ function M.config()
     let g:tex_flavor='latex'
     let g:vimtex_view_method='zathura'
     let g:vimtex_quickfix_mode=0
+    let g:vimtex_compiler_progname='nvr'
     " set conceallevel=1
     " let g:tex_conceal='abdmg'
     ]])
